6ff53bf1003adb913a150d5ae6b1ad9d2e159b88
SimoneColombo1 <simcolombo2@gmail.com>
2024-10-02 19:59:45 +0200

    Creata la struttura del jumbo

---
+++ app/components/HomePage/jumbo.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import style from  "../../styles/home/jumbo.scss";
+
+export default function Jumbo() {
+  return (
+  <section class="jumbo">
+  <div class="text">
+      <h1>Chi siamo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem accusantium illum ipsa commodi repellat minus, est aspernatur delectus obcaecati magnam quas, possimus itaque quo consequuntur quaerat deserunt voluptate quia suscipit nemo corrupti reiciendis consectetur? Tenetur, molestias est a magnam voluptas soluta dolores obcaecati cum, ipsum illo hic beatae possimus recusandae quibusdam harum sequi nulla qui aperiam magni odit ad sapiente quaerat quas! Id voluptatum quaerat blanditiis? Harum quos nobis, nisi aut molestias quae a quis asperiores recusandae quam magnam ab.</p></div>
+  <div class="img"> <h1>IMMAGINE</h1></div>
+  </section>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Jumbo from "./components/HomePage/jumbo";
 
 
 export default function Home() {
   return (
   <section class="HomePage">
-    
+    <Jumbo/>
   </section>
   );
 }
